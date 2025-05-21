6a5f9539d5de0733fd0fb6387da1deb38317a353
Raymond930103 <raymond2040103@gmail.com>
2025-05-21 04:41:34 +0000

    change ports

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins:[react()],
+  server:{ host:true, port:5173 }  // host:true 才能被容器外訪問
+});
